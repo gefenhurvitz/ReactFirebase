bb4694370b977f36d59aa2d3d3076cae2b788c57
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2024-06-07 01:49:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, {useRef , useState} from 'react'
 import './App.css';
 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import {useAuthState, useSignInWithGoogle} from 'react-firebase-hooks/auth'
+import {useCollectionData} from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCDyQzl-f54nklx2qpQyD3cUzWXzLszYwU",
+  authDomain: "react-firebase-gefen.firebaseapp.com",
+  projectId: "react-firebase-gefen",
+  storageBucket: "react-firebase-gefen.appspot.com",
+  messagingSenderId: "1069605619148",
+  appId: "1:1069605619148:web:43bdf241dba24e7bbb8969",
+  measurementId: "G-MK3KLXNYK5"
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+ 
+
+ 
 function App() {
+
+  const [user] = useAuthState(auth)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <h1>Hello</h1>
+      <SignOut/>
+      <section>
+        {user ? <ChatRoom /> : <SignIn/>}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.googleAuthProvider()
+    auth.useSignInwithPopup(provider)
+  }
+  return(
+    <button onClick={SignInWithGoogle}>sign in with google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <bottun onClick={()=> auth.SignOut}>Sign Out</bottun>
+  )
+}
+
+function ChatRoom() {
+
 }
 
 export default App;
